a20a4e600aa5467ff473ac39890005b7c1dff421
klawingco <marcjhon18@gmail.com>
2022-01-07 13:31:55 +0800

    feature(axios): Issue #9 #14

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,56 @@
 
 import { IMAL, IMALClient, APIFetcher } from './types'
-import { apiFetcher } from './lib/api-adapter'
+import { initApiClient, apiFetcher } from './lib/api-adapter'
 import Auth from './lib/authentication'
 import initAnime from './lib/anime'
 import initManga from './lib/manga'
 import initForum from './lib/forum'
 import initUserProfile from './lib/user'
+import { AxiosResponse, AxiosRequestConfig } from 'axios'
 
 export const createClient = (clientOpts: IMALClient) => {
-  
+
   let nsfw = false
+
+  const axiosInstance = initApiClient(clientOpts);
+
   // Build the apiFetcher based from options
   const apiRequest = (apiOpts: APIFetcher) =>
-    apiFetcher(apiOpts, { ...clientOpts, nsfw})
+    apiFetcher(axiosInstance, apiOpts, { ...clientOpts, nsfw })
 
   return {
     ...initAnime(apiRequest),
     ...initManga(apiRequest),
     ...initForum(apiRequest),
     ...initUserProfile(apiRequest),
-    allowNSFW: function(){
-      nsfw = true;
-      return this;
-    }
 
+    // Extension functions
+    allowNSFW: function () {
+      nsfw = true
+      return this
+    },
+    setRequestLogger: function (
+      loggerCb?: (response: AxiosRequestConfig) => void
+    ) {
+      if (loggerCb) {
+        axiosInstance.interceptors.request.use((request) => {
+          loggerCb(request)
+          return request
+        })
+      }
+      return this
+    },
+    setResponseLogger: function (
+      loggerCb?: (response: AxiosResponse<any>) => void
+    ) {
+      if (loggerCb) {
+        axiosInstance.interceptors.response.use((response) => {
+          loggerCb(response)
+          return response
+        })
+      }
+      return this
+    },
   }
 }
 
